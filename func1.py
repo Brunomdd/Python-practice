b516c36ec44c8f8e438c09743f6dca4d81ba6d09
Brunomdd <brunnodiniiz@gmail.com>
2025-09-25 14:51:32 -0300

    Adicionar função que imprime saudação com nome

---
+++ func1.py
@@ -0,0 +1,5 @@
+#crie uma função chamada funcao que receba um parâmetro nome e imprima a mensagem Olá, <nome>, substituindo <nome> pelo valor recebido.
+
+def funcao(nome):
+    print(f'Ola {nome}')
+funcao('Bruno')
